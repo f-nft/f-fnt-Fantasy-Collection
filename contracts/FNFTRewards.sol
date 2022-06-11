eb8ce746fa4f372b40154555f8d14f9c21c5e11f
Elliot <pnft56061@gmail.com>
2022-06-11 10:22:56 +0700

    main

--- contracts/FNFTRewards.sol
+++ contracts/FNFTRewards.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT LICENSE
 
 pragma solidity 0.8.4;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
