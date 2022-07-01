478260b512c46ca4653e58723c20219db71b25f3
f-nft <100182093+f-nft@users.noreply.github.com>
2022-07-01 10:04:59 +0700

    wallet

--- website/src/AppFunctional.js
+++ website/src/AppFunctional.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+
 // eslint-disable-next-lineimport './App.css';
-// App.js;
+// App.js
 import "./App.css";
 import "./index.css";
 import { Button, ButtonGroup } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
 import "sf-font";
 import axios from "axios";
-import ABI from "./ABI.json";
-import VAULTABI from "./VAULTABI.json";
-import TOKENABI from "./TOKENABI.json";
-import { NFTCONTRACT, STAKINGCONTRACT, polygonscanapi, moralisapi } from "./config";
+import ABI from "./config/ABI.json";
+import VAULTABI from "./config/VAULTABI.json";
+import TOKENABI from "./config/TOKENABI.json";
+import { NFTCONTRACT, STAKINGCONTRACT, polygonscanapi, moralisapi } from "./config/config";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import WalletLink from "walletlink";
@@ -19,6 +20,100 @@ import moving from "./images/moving.gif";
 import ListNft from "./components/Listnft";
 import { ethers } from "ethers";
 const { ethereum } = window;
+import detectEthereumProvider from '@metamask/detect-provider';
+
+// metamask provider
+const provider = await detectEthereumProvider();
+
+if (provider) {
+  startApp(provider); // Initialize your app
+} else {
+  console.log('Please install MetaMask!');
+}
+
+function startApp(provider) {
+  // If the provider returned by detectEthereumProvider is not the same as
+  // window.ethereum, something is overwriting it, perhaps another wallet.
+  if (provider !== window.ethereum) {
+    console.error('Do you have multiple wallets installed?');
+  }
+  // Access the decentralized web!
+}
+
+/**********************************************************/
+/* Handle chain (network) and chainChanged (per EIP-1193) */
+/**********************************************************/
+
+const chainId = await ethereum.request({ method: 'eth_chainId' });
+handleChainChanged(chainId);
+
+ethereum.on('chainChanged', handleChainChanged);
+
+function handleChainChanged(_chainId) {
+  // We recommend reloading the page, unless you must do otherwise
+  window.location.reload();
+}
+
+/***********************************************************/
+/* Handle user accounts and accountsChanged (per EIP-1193) */
+/***********************************************************/
+
+let currentAccount = null;
+ethereum
+  .request({ method: 'eth_accounts' })
+  .then(handleAccountsChanged)
+  .catch((err) => {
+    // Some unexpected error.
+    // For backwards compatibility reasons, if no accounts are available,
+    // eth_accounts will return an empty array.
+    console.error(err);
+  });
+
+// Note that this event is emitted on page load.
+// If the array of accounts is non-empty, you're already
+// connected.
+ethereum.on('accountsChanged', handleAccountsChanged);
+
+// For now, 'eth_accounts' will continue to always return an array
+function handleAccountsChanged(accounts) {
+  if (accounts.length === 0) {
+    // MetaMask is locked or the user has not connected any accounts
+    console.log('Please connect to MetaMask.');
+  } else if (accounts[0] !== currentAccount) {
+    currentAccount = accounts[0];
+    // Do any other work!
+  }
+}
+
+/*********************************************/
+/* Access the user's accounts (per EIP-1102) */
+/*********************************************/
+
+// You should only attempt to request the user's accounts in response to user
+// interaction, such as a button click.
+// Otherwise, you popup-spam the user like it's 1999.
+// If you fail to retrieve the user's account(s), you should encourage the user
+// to initiate the attempt.
+document.getElementById('connectButton', connect);
+
+// While you are awaiting the call to eth_requestAccounts, you should disable
+// any buttons the user can click to initiate the request.
+// MetaMask will reject any additional requests while the first is still
+// pending.
+function connect() {
+  ethereum
+    .request({ method: 'eth_requestAccounts' })
+    .then(handleAccountsChanged)
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+};
 
 var account = null;
 var contract = null;
@@ -38,7 +133,12 @@ const providerOptions = {
     },
     walletlink: {
         package: WalletLink,
-        options: { appName: "f-nft Polygon", infuraId: "50f6635fbcc742f18ce7a2a5cbe73ffa", rpc: "", chainId: 137, appLogoUrl: null, darkMode: true, },
+        options: { 
+            appName: "f-nft Polygon", 
+            infuraId: "50f6635fbcc742f18ce7a2a5cbe73ffa", 
+            rpc: "", chainId: 137, 
+            appLogoUrl: null, 
+            darkMode: true, },
     },
 };
 
