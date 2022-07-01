6d912fdc6418d36e75b1ee4ad9d212265699a8eb
f-nft <100182093+f-nft@users.noreply.github.com>
2022-07-01 10:18:03 +0700

    debug

--- website/src/index.js
+++ website/src/index.js
@@ -3,10 +3,12 @@ import * as ReactDOM from 'react-dom/client';
 import './App.css';
 import './index.css';
 import AppFunctional from './AppFunctional';
+import connect from './components/Metamask';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <connect />
         <AppFunctional />
     </React.StrictMode>
 );
