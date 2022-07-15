091d3af87c6c748438cc2c537330e5fbeb094b02
Baqir Naqvi <b.naqvi99@gmail.com>
2022-07-15 13:20:59 +0500

    ReDesign 1.1.3

---
+++ website/src/components/ImagesImport.jsx
@@ -0,0 +1,16 @@
+import image1 from '../images/asset7.png'
+import image2 from '../images/asset8.png'
+import image3 from '../images/asset9.png'
+import image4 from '../images/asset10.png'
+import image5 from '../images/asset11.png'
+import image6 from '../images/asset12.png'
+
+
+
+const IMAGES={
+   image1,image2,image3,image4,image5,image6
+
+}
+
+
+export default IMAGES;
